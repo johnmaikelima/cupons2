60c84f95937c6f5b617896e5238d3e309d4a7f1d
johnmaikelima <johnpatrocinio1@gmail.com>
2025-05-06 21:34:37 -0300

    Initial commit: Backup do projeto Cupons

--- backup2/src/lib/storeDescription.ts
+++
@@ -1,3 +0,0 @@
-export function generateStoreDescription(storeName: string): string {
-  return `Encontre os melhores cupons e ofertas de ${storeName}. Economize em suas compras com descontos exclusivos.`;
-}
